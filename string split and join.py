5beb080cfc6bfbc0834fdf7c0e82d5cce96ac109
Manikandanravikumar <101857943+Manikandanravikumar@users.noreply.github.com>
2022-04-07 19:52:41 +0530

    Create string split and join.py

---
+++ string split and join.py
@@ -0,0 +1,8 @@
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+if __name__ == '__main__':
+    line = raw_input()
+    result = split_and_join(line)
+    print result
